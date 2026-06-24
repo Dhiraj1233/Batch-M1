07b6b802dc7a5e8303cbc6ff0f5fd5dac44ea0a9
DK <dhirajroy112233@gmail.com>
2026-06-24 14:11:54 +0530

    HHH

---
+++ src/test/java/com/AutomationExcercise/Utility/BaseClass.java
@@ -0,0 +1,29 @@
+package com.AutomationExcercise.Utility;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass 
+{
+	public WebDriver driver;
+	@BeforeTest
+	public void setUp() 
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("http://automationexercise.com");
+		
+	}
+	@AfterTest
+	public void tearDown() 
+	{
+//		driver.quit();
+		
+	}
+
+}
